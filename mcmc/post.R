ba78abab7668ce9859ec0914551fd80bae0caf53
Andrei Kramer <andreikr@kth.se>
2025-08-28 22:31:38 +0200

    made a better guess about the auto-correlation when not having uwerr

--- mcmc/post.R
+++ mcmc/post.R
@@ -19,17 +19,21 @@ sampleQuality <- function(x){
 	N <- length(l)
 	## use a sub-sample of reduced size, omitting the burn-in
 	## and calculate the auto-correlation length
+	sub <- seq(ceiling(N*0.75),N)
 	if (requireNamespace("hadron")){
-		sub <- seq(ceiling(N*0.75),N)
 		res <- hadron::uwerr(data=l[sub],pl=TRUE)
 		cat("tau: ",res$tauint,"±",res$dtauint,"\n")
 		tau <- errors::set_errors(res$tauint,res$dtauint)
-	} else {
-		a <- 1-mean(diff(sort(l))<1e-15+1e-15*max(l))
-		tau <- 1.0/a
-		tau <- errors::set_errors(tau,0.1*tau + 5) ## a wild guess
+	} else  {
+		A <- acf(l[sub],lag.max=round(length(sub)/2))
+		a <- as.numeric(A$acf)
+		tau <- sum(a[a>0.1])
+		tau <- errors::set_errors(tau,1)
+		## TODO: find out what the error is in this case
 	}
-	a <- mean(as.numeric(diff(sort(l))>1e-13))
+
+	a <- 1-mean(diff(sort(l))<1e-15+1e-15*max(l))
+	message(sprintf("acceptance^-1: %g",solve(a)))
 
 	s <- uqsa::simulator.c(ex,modelName,parMap=uqsa::log10ParMap)
 	i <- seq(ceiling(N/4),N,by=ceiling(as.numeric(tau)+1.0/a))
